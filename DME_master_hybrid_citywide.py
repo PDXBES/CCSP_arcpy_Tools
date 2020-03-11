191b422663c666ab0a6bb75af3d07607d76b2d48
Dan Ashney <daniel.ashney@portlandoregon.gov>
2020-03-11 14:20:48 -0700

    fixed - was using ccsp version of method in city wide version

--- DME_master_hybrid_citywide.py
+++ DME_master_hybrid_citywide.py
@@ -11,18 +11,17 @@ config = Config('TEST')
 dme_master_hybrid = DmeMasterHybrid(config)
 dme_master_hybrid_db_data_io = DmeMasterHybridDbDataIo(config)
 
-## will write out result with citywide extent
+## will write out result with citywide extent (DME_master_hybrid)
 ## 1. create in memory version of DME links
-## 2. create in memory version of EMGAATS links which have already been subset to project area
-## 3. apply decision logic to choose between DME and master link values where global_ids match
-## 4. append result to CCSP.DME_MASTER_HYBRID
+## 2. apply decision logic to choose between DME and master link values where global_ids match
+## 3. append result to CCSP.DME_MASTER_HYBRID
 
 start = time.time()
 test_dme_links = dme_master_hybrid.create_dme_links(dme_master_hybrid_db_data_io)
 print time.time() - start
 
 start = time.time()
-test_master_links = dme_master_hybrid.create_ccsp_master_links(dme_master_hybrid_db_data_io)
+test_master_links = dme_master_hybrid.create_master_links(dme_master_hybrid_db_data_io)
 print time.time() - start
 
 dme_master_links = []
