9e44c33455743fd7a0cd11c4ff093ced4ef88534
Dan Ashney <daniel.ashney@portlandoregon.gov>
2020-09-22 11:47:17 -0700

    minor - got rid of weird duplication

--- businessclasses/config.py
+++ businessclasses/config.py
@@ -13,12 +13,8 @@ class Config:
 
         self.test_flag = test_flag
 
-        #TODO - both sde_connections are needed or else it bonks - find out how to use only one
-        #TODO - prob have data_loader inherit config just like utility does
-        sde_connections = r"\\besfile1\CCSP\03_WP2_Planning_Support_Tools\03_RRAD\CCSP_Data_Management_ToolBox\connection_files"
         self.sde_connections = r"\\besfile1\CCSP\03_WP2_Planning_Support_Tools\03_RRAD\CCSP_Data_Management_ToolBox\connection_files"
         self.ETL_load_base_folder = r"\\besfile1\ccsp\03_WP2_Planning_Support_Tools\04_CostEstimator\Code\InputGDB"
-        #self.ETL_source_json = r"\\besfile1\CCSP\03_WP2_Planning_Support_Tools\04_CostEstimator\Code\InputGDB\ETL_input_data_sources.json"
 
         ##
         server = None
@@ -34,15 +30,15 @@ class Config:
         CCSP_sde = server + ".CCSP.sde"
         BESGEORPT_sde = server + ".BESGEORPT.sde"
 
-        self.egh_public = os.path.join(sde_connections, GISDB1)
+        self.egh_public = os.path.join(self.sde_connections, GISDB1)
         self.DME_table_name = r"EGH_Public.ARCMAP_ADMIN.collection_lines_bes_pdx"
         self.DME_sde_path = self.egh_public + r"\\" + self.DME_table_name
 
-        self.EMGAATS_sde_path = os.path.join(sde_connections, EMGAATS_sde)
+        self.EMGAATS_sde_path = os.path.join(self.sde_connections, EMGAATS_sde)
         self.master_links_sde_path = self.EMGAATS_sde_path + r"\EMGAATS.GIS.Links"
         self.master_nodes_sde_path = self.EMGAATS_sde_path + r"\EMGAATS.GIS.Nodes"
 
-        self.CCSP_sde_path = os.path.join(sde_connections, CCSP_sde)
+        self.CCSP_sde_path = os.path.join(self.sde_connections, CCSP_sde)
         self.DME_view_name = r"CCSP.GIS.VDME"
         self.Master_links_nodes_view_name = r"CCSP.GIS.V_master_link_with_nodes"
         self.CCSP_Master_links_nodes_view_name = r"CCSP.GIS.v_ccsp_master_link_with_nodes"
@@ -51,7 +47,7 @@ class Config:
         self.DME_master_hybrid_sde_path = self.CCSP_sde_path + r"\CCSP.GIS.DME_master_hybrid"
 
         ## placeholders for use once we switch to using view/QL instead of hard coded query in dme_master_hybrid.create_dme_links()
-        self.BESGEORPT_sde_path = os.path.join(sde_connections, BESGEORPT_sde)
+        self.BESGEORPT_sde_path = os.path.join(self.sde_connections, BESGEORPT_sde)
         self.collection_lines_ACTIVE_BES_SEWER_STORM_view_name = r"\BESGEORPT.GIS.v_collection_lines_ACTIVE_BES_SEWER_STORM"
         self.collection_lines_ACTIVE_BES_SEWER_STORM_view_name_path = self.BESGEORPT_sde_path + self.collection_lines_ACTIVE_BES_SEWER_STORM_view_name
 
