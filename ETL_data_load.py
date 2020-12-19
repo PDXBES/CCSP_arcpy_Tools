044441ec69024fac0789556a22b5b3dfbbe99509
Dan Ashney <daniel.ashney@portlandoregon.gov>
2020-12-18 17:18:58 -0800

    added logging functionality, seems to work well - hoping this will help to know what/when/if process ran after setup on appserver

--- ETL_data_load.py
+++ ETL_data_load.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import arcpy
 import DME_master_hybrid_citywide
 #from dataio.utility import Utility
-#from businessclasses import config
+from businessclasses import config
 
 #appsettings_file = r"c:\temp\working\appsettings.json"
 appsettings_file = r"\\besfile1\CCSP\03_WP2_Planning_Support_Tools\04_CostEstimator\Code\InputGDB\CCSP_Tools_Input\appsettings.json"
@@ -14,33 +14,33 @@ test_flag = "PROD"
 
 data_load = DataLoad()
 utility = data_load.utility
+config = config.Config(test_flag)
 
-print "ETL Data Loader - Process started"
-print datetime.today().strftime("%m/%d/%Y, %H:%M:%S")
+log_obj = utility.Logger(config.log_file)
+
+log_obj.info("ETL Data Loader - Process started".format())
 
 try:
 
-    print "DME master hybrid cleanup - " + datetime.today().strftime("%H:%M:%S")
+    log_obj.info("DME master hybrid cleanup".format())
     utility.DME_master_hybrid_data_cleanup()
 
-    print "Creating DME master hybrid - " + datetime.today().strftime("%H:%M:%S")
+    log_obj.info("Creating DME master hybrid".format())
     DME_master_hybrid_citywide.create_citywide_hybrid()
 
-    print "Creating intermediate gdb - " + datetime.today().strftime("%H:%M:%S")
+    log_obj.info("Creating intermediate gdb".format())
     data_load.create_gdb(utility.intermediate_gdb_full_path_name())
 
-    print "Loading Data - " + datetime.today().strftime("%H:%M:%S")
+    log_obj.info("Loading Data".format())
     data_load.load_data(appsettings_file, data_source_file)
 
-    #Q: want to rename load.gdb instead of del? - that would give access to 1 version prior
-
-    print "GDB cleanup - " + datetime.today().strftime("%H:%M:%S")
-    print "     delete existing load gdb"
+    log_obj.info("GDB cleanup".format())
+    log_obj.info("     delete existing load gdb".format())
     utility.delete_dir(utility.ccsp_gdb_full_path_name())
-    print "     rename intermediate to load gdb"
+    log_obj.info("     rename intermediate to load gdb".format())
     utility.rename_intermediate_gdb_to_input_gdb()
 
-    print "Zipping gdb - " + datetime.today().strftime("%H:%M:%S")
+    log_obj.info("Zipping gdb".format())
     utility.zip(utility.ccsp_gdb_full_path_name())
 
 except:
@@ -48,8 +48,7 @@ except:
     utility.delete_dir(utility.intermediate_gdb_full_path_name())
     utility.delete_file(utility.intermediate_gdb_full_path_name() + ".zip")
 
-    arcpy.AddError("Data could not be loaded")
+    log_obj.info("Data could not be loaded".format())
     arcpy.ExecuteError()
 
-print "Data Loader - Process ended"
-print datetime.today().strftime("%m/%d/%Y, %H:%M:%S")
+log_obj.info("Data Loader - Process ended".format())
--- dataio/utility.py
+++ dataio/utility.py
@@ -4,6 +4,9 @@ from datetime import datetime
 import arcpy
 import zipfile
 import shutil
+import logging
+import sys
+import logging.config
 
 class Utility:
     def __init__(self, config):
@@ -107,3 +110,22 @@ class Utility:
             except:
                 print("  unable to truncate, using Delete Rows")
                 arcpy.DeleteRows_management(feature_class)
+
+    # https://stackoverflow.com/questions/6386698/how-to-write-to-a-file-using-the-logging-python-module
+    def Logger(self, file_name):
+        formatter = logging.Formatter(fmt='%(asctime)s %(module)s,line: %(lineno)d %(levelname)8s | %(message)s',
+                                      datefmt='%Y/%m/%d %H:%M:%S')  # %I:%M:%S %p AM|PM format
+        logging.basicConfig(filename='%s.log' % (file_name),
+                            format='%(asctime)s %(module)s,line: %(lineno)d %(levelname)8s | %(message)s',
+                            datefmt='%Y/%m/%d %H:%M:%S', filemode='a', level=logging.INFO)
+        log_obj = logging.getLogger()
+        log_obj.setLevel(logging.DEBUG)
+        # log_obj = logging.getLogger().addHandler(logging.StreamHandler())
+
+        # console printer
+        screen_handler = logging.StreamHandler(stream=sys.stdout)  # stream=sys.stdout is similar to normal print
+        screen_handler.setFormatter(formatter)
+        logging.getLogger().addHandler(screen_handler)
+
+        log_obj.info("Starting log session..")
+        return log_obj
